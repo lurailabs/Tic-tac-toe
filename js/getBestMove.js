c428c4889f2b55d8ccc35e258cf9e9479777d013
lurailabs.com <admin@lurailabs.com>
2016-03-08 22:56:18 +0100

    Max. depth added to algorithm so it works with larger boards

--- js/getBestMove.js
+++ js/getBestMove.js
@@ -4,18 +4,26 @@
 function getBestMove() {
 
     var possibleMoves = game.getPossibleMoves(state);
+
+    // add a little randomness to list of possible moves
+    possibleMoves = possibleMoves.sort(function() {
+        return .5 - Math.random();
+    });
+
     var best = -10000;
     var aux;
     var position = possibleMoves[0];
 
-
     for (var i = 0; i < possibleMoves.length; i++) {
         state[possibleMoves[i]] = game.player.pc;
-        aux = min();
+        aux = min(0);
+
         if (aux > best) {
             best = aux;
             position = possibleMoves[i];
         }
+        if (best >= 1) return position;
+
         state[possibleMoves[i]] = null;
     }
     return position;
@@ -24,7 +32,7 @@ function getBestMove() {
 
 /******************************************************************************/
 
-function max() {
+function max(depth) {
 
     var terminalState = game.checkTerminal(state);
 
@@ -37,7 +45,7 @@ function max() {
 
     for (var k = 0; k < possibleMoves.length; k++) {
         state[possibleMoves[k]] = game.player.pc;
-        aux = min();
+        aux = min(depth);
         if (aux > best) best = aux;
 
         state[possibleMoves[k]] = null;
@@ -48,7 +56,8 @@ function max() {
 
 /******************************************************************************/
 
-function min() {
+function min(depth) {
+
     var terminalState = game.checkTerminal(state);
     if (terminalState === game.player.pc) return 1;
     if (terminalState === game.player.human || terminalState === 'tie') return 0;
@@ -57,9 +66,12 @@ function min() {
     var best = 10000;
     var aux;
 
+    depth++;
+    if (game.getLevel() > 3 && depth > 2) return best;   // max depth in 3x3 is 3
+
     for (var j = 0; j < possibleMoves.length; j++) {
         state[possibleMoves[j]] = game.player.human;
-        aux = max();
+        aux = max(depth);
         if (aux < best) best = aux;
 
         state[possibleMoves[j]] = null;
